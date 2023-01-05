c3c9a236c35d58125d75fa55d0bec265ccd07093
Asif Kazmi <akazmi001@gmail.com>
2023-01-04 23:59:08 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Reads a text file and returns the list of items"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
